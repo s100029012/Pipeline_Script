099e16bce373a948f4c66089f1dd6980791e6460
Hsiang-An Wang <s100029012@gmail.com>
2021-07-21 16:01:08 +0800

    Add start-app script

---
+++ start-app.bat
@@ -0,0 +1,5 @@
+@echo off
+set BUILD_ID=DontKillMe
+rem d:
+rem cd D:\Progs\services
+start /B %JAVA_HOME%/bin/java -jar spring-boot/build/libs/spring-boot-0.0.1-SNAPSHOT.jar > %CD%\apps\logs\app.log 2>&1
