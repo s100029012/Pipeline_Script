dccde7c2b5b7dbfc5a39151ce14bf6344596c96e
Hsiang-An Wang <s100029012@gmail.com>
2021-07-22 11:19:44 +0800

    Append console output to log file.

--- start-app.bat
+++ start-app.bat
@@ -3,4 +3,4 @@ set BUILD_ID=DontKillMe
 rem d:
 rem cd D:\Progs\services
 mkdir %CD%\apps\logs
-start /B %JAVA_HOME%/bin/java -jar spring-boot/build/libs/spring-boot-0.0.1-SNAPSHOT.jar > %CD%\apps\logs\app.log 2>&1
+start /B %JAVA_HOME%/bin/java -jar spring-boot/build/libs/spring-boot-0.0.1-SNAPSHOT.jar >> %CD%\apps\logs\app.log 2>&1
